48d58e6a33014d6e24dabff6c0ca0506982110c0
Julie Lerman <julielerman@users.noreply.github.com>
2021-01-27 14:17:40 -0500

    exercise files for m9

---
+++ M9 Tests/Console Before/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name//,
+                                                 //DbLoggerCategory.Database.Transaction.Name
+                                               },
+                       LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Samurai>()
+             .HasMany(s => s.Battles)
+             .WithMany(b => b.Samurais)
+             .UsingEntity<BattleSamurai>
+              (bs => bs.HasOne<Battle>().WithMany(),
+               bs => bs.HasOne<Samurai>().WithMany())
+             .Property(bs => bs.DateJoined)
+             .HasDefaultValueSql("getdate()");
+
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+            modelBuilder.Entity<SamuraiBattleStat>().HasNoKey().ToView("SamuraiBattleStats");
+        }
+    }
+}
